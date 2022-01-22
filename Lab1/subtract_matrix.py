60aa656f301cf90158e481c173b9b191d19378ff
Yousuf Uyghur <44427180+alfen-yu@users.noreply.github.com>
2022-01-22 22:57:01 +0500

    Added files via upload
    Lab 1, Question 1 
    Data Structures and Algorithms

---
+++ Lab1/subtract_matrix.py
@@ -0,0 +1,38 @@
+import ast
+A = input()
+A = ast.literal_eval(A)
+B = input()
+B = ast.literal_eval(B)
+
+
+# CUSTOM INPUTS
+# [[1,2,3],[4,5,6],[7,8,9]]
+# [[9,8,7],[6,5,4],[3,2,1]]
+
+
+def matrix_subtraction(a, b):
+    rows_a = len(a)
+
+    c, d = [], []
+
+    for i in a:
+        for x in i:
+            c.append(x)
+    for i in b:
+        for x in i:
+            d.append(x)
+
+    if len(c) == len(d):
+        count = 0
+        lst = []
+
+        for i in range(rows_a):
+            lst.append([])
+            for x in range(len(a[0])):
+                lst[i].append(c[count] - d[count])
+                count += 1
+        return lst
+    else:
+        return "Matrices A and B don't have the same dimension required for matrix subtraction."
+        
+print(matrix_subtraction(A,B))
